91cc61571f652d8689c02d6103e41edb4f12cad5
Martin Ezequiel Langberg <martinlangberg@gmail.com>
2016-07-23 19:38:33 -0300

    only dotfiles

--- add_repos.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-readarray repos < repositories.txt
-for p in ${!repos[@]}
-do
-  read -p "Add ${repos[$p]} PPA? (y/n) " reply
-  [ "${reply,,}" = "y" ] && add-apt-repository -y "$p" 
-done
